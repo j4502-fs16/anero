00b188c9e384601a655acaf1195322c46e4b5937
amandanero <arn5x7@mail.missouri.edu>
2016-11-10 10:51:22 -0600

    trying to create hamburger menu/get my #h1 to move

---
+++ JavaScript examples w12/jquery_class/finished.js
@@ -0,0 +1,15 @@
+//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500); //these are by miliseconds
+   //don't know if this should be here
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+	   if (  $(window).width() > 700 ) { //when this is the case, this will happen... = if
+		$('nav ul').removeAttr('style');
+	 }
+	});//end resize
+});//end ready
